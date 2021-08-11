a84ebbfac2064588190d6444503a7ef82e6df4c4
INODE64 <ffelix@inode64.com>
2021-08-11 12:57:01 +0200

    * Prepare for split in two script (Cache / Cookies and History), to run separately * Fix name of function Clear-EdgeCacheFiles * Include more folder for clear in chrome and Windows * Include support for Thunderbird, Adobe acrobat, iTunes and Libreoffice * Fix a problem that files are never deleted in InetCache

--- DriveClean.ps1
+++ DriveClean.ps1
@@ -16,11 +16,15 @@ Function Clear-UserCacheFiles {
     # Stop-BrowserSessions
     ForEach($localUser in (Get-ChildItem 'C:\users').Name)
     {
-        Clear-ChromeCache $localUser
+        Clear-ChromeCacheFiles $localUser
         Clear-EdgeCacheFiles $localUser
         Clear-FirefoxCacheFiles $localUser
+        Clear-ThunderbirdCacheFiles $localUser
         Clear-WindowsUserCacheFiles $localUser
         Clear-TeamsCacheFiles $localUser
+        Clear-iTunesCacheFiles $localUser
+        Clear-AcrobatCacheFiles $localUser
+        Clear-LibreOfficeCacheFiles $localUser
     }
 }
 
@@ -30,9 +34,10 @@ Function Clear-UserCacheFiles {
 Function Clear-WindowsUserCacheFiles {
     param([string]$user=$env:USERNAME)
     Remove-CacheFiles "C:\Users\$user\AppData\Local\Temp"
+    Remove-CacheFiles "C:\Users\$user\AppData\Local\Microsoft\Internet Explorer\Tiles"
     Remove-CacheFiles "C:\Users\$user\AppData\Local\Microsoft\Windows\WER"
     Remove-CacheFiles "C:\Users\$user\AppData\Local\Microsoft\Windows\INetCache"
-    Remove-CacheFiles "C:\Users\$user\AppData\Local\Microsoft\Windows\INetCookies"
+    Remove-CacheFiles "C:\Users\$user\AppData\Local\Microsoft\Windows\INetCache\Low"
     Remove-CacheFiles "C:\Users\$user\AppData\Local\Microsoft\Windows\IECompatCache"
     Remove-CacheFiles "C:\Users\$user\AppData\Local\Microsoft\Windows\IECompatUaCache"
     Remove-CacheFiles "C:\Users\$user\AppData\Local\Microsoft\Windows\IEDownloadHistory"
@@ -123,12 +128,12 @@ Function Remove-CacheFiles {
 #------------------------------------------------------------------#
 #- Clear-ChromeCache                                               #
 #------------------------------------------------------------------#
-Function Clear-ChromeCache {
+Function Clear-ChromeCacheFiles {
     param([string]$user=$env:USERNAME)
     if((Test-Path "C:\users\$user\AppData\Local\Google\Chrome\User Data\Default"))
     {
         $chromeAppData = "C:\Users\$user\AppData\Local\Google\Chrome\User Data\Default" 
-        $possibleCachePaths = @('Cache','Cache2\entries\','Cookies','History','Top Sites','VisitedLinks','Web Data','Media Cache','Cookies-Journal','ChromeDWriteFontCache')
+        $possibleCachePaths = @('Cache','Cache2\entries\','Code Cache','GPUCache','Service Worker','Top Sites','VisitedLinks','Web Data','Media Cache','ChromeDWriteFontCache')
         ForEach($cachePath in $possibleCachePaths)
         {
             Remove-CacheFiles "$chromeAppData\$cachePath"
@@ -139,17 +144,17 @@ Function Clear-ChromeCache {
 #------------------------------------------------------------------#
 #- Clear-EdgeCache                                                 #
 #------------------------------------------------------------------#
-Function Clear-EdgeCache {
+Function Clear-EdgeCacheFiles {
     param([string]$user=$env:USERNAME)
     if((Test-Path "C:\Users$user\AppData\Local\Microsoft\Edge\User Data\Default"))
     {
         $EdgeAppData = "C:\Users$user\AppData\Local\Microsoft\Edge\User Data\Default"
-        $possibleCachePaths = @('Cache','Cache2\entries','Cookies','History','Top Sites','Visited Links','Web Data','Media History','Cookies-Journal')
+        $possibleCachePaths = @('Cache','Cache2\entries','Top Sites','Visited Links','Web Data','Media History')
         ForEach($cachePath in $possibleCachePaths)
         {
             Remove-CacheFiles "$EdgeAppData$cachePath"
         }
-        }
+    }
 }
 
 #Endregion ChromiumBrowsers
@@ -163,7 +168,7 @@ Function Clear-FirefoxCacheFiles {
     param([string]$user=$env:USERNAME)
     if((Test-Path "C:\users\$user\AppData\Local\Mozilla\Firefox\Profiles"))
     {
-        $possibleCachePaths = @('cache','cache2\entries','thumbnails','cookies.sqlite','webappsstore.sqlite','chromeappstore.sqlite')
+        $possibleCachePaths = @('cache','cache2\entries','thumbnails','webappsstore.sqlite','chromeappstore.sqlite')
         $firefoxAppDataPath = (Get-ChildItem "C:\users\$user\AppData\Local\Mozilla\Firefox\Profiles" | Where-Object { $_.Name -match 'Default' }[0]).FullName 
         ForEach($cachePath in $possibleCachePaths)
         {
@@ -179,7 +184,7 @@ Function Clear-WaterfoxCacheFiles {
     param([string]$user=$env:USERNAME)
     if((Test-Path "C:\users\$user\AppData\Local\Waterfox\Profiles"))
     {
-        $possibleCachePaths = @('cache','cache2\entries','thumbnails','cookies.sqlite','webappsstore.sqlite','chromeappstore.sqlite')
+        $possibleCachePaths = @('cache','cache2\entries','thumbnails','webappsstore.sqlite','chromeappstore.sqlite')
         $waterfoxAppDataPath = (Get-ChildItem "C:\users\$user\AppData\Local\Waterfox\Profiles" | Where-Object { $_.Name -match 'Default' }[0]).FullName
         ForEach($cachePath in $possibleCachePaths)
         {
@@ -211,6 +216,81 @@ Function Clear-TeamsCacheFiles {
 }
 
 #Endregion CommunicationPlatforms
+
+#Region MiscApplications
+
+#------------------------------------------------------------------#
+#- Clear-ThunderbirdCacheFiles                                     #
+#------------------------------------------------------------------#
+Function Clear-ThunderbirdCacheFiles {
+    param([string]$user=$env:USERNAME)
+    if((Test-Path "C:\users\$user\AppData\Local\Thunderbird\Profiles"))
+    {
+        $possibleCachePaths = @('cache','cache2\entries')
+        $ThunderbirdAppDataPath = (Get-ChildItem "C:\users\$user\AppData\Local\Thunderbird\Profiles" | Where-Object { $_.Name -match 'Default' }[0]).FullName 
+        ForEach($cachePath in $possibleCachePaths)
+        {
+            Remove-CacheFiles "$ThunderbirdAppDataPath\$cachePath"
+        }
+    } 
+}
+
+#------------------------------------------------------------------#
+#- CleariTunesCacheFiles                                           #
+#------------------------------------------------------------------#
+Function Clear-iTunesCacheFiles { 
+    param([string]$user=$env:USERNAME)
+    if((Test-Path "C:\users\$user\AppData\Local\Apple Computer\iTunes"))
+    {
+	$iTunesAppDataPath = "C:\users\$user\AppData\Local\Apple Computer\iTunes"
+        $possibleCachePaths = @('SubscriptionPlayCache')
+        ForEach($cachePath in $possibleCachePaths)
+        {
+            Remove-CacheFiles "$iTunesAppDataPath\$cachePath"
+        }
+    }   
+}
+
+#------------------------------------------------------------------#
+#- Clear-AcrobatCacheFiles                                     #
+#------------------------------------------------------------------#
+Function Clear-AcrobatCacheFiles {
+    param([string]$user=$env:USERNAME)
+    $DirName="C:\users\$user\AppData\LocalLow\Adobe\Acrobat"
+    if((Test-Path "$DirName"))
+    {
+        $possibleCachePaths = @('Cache','ConnectorIcons')
+	ForEach($AcrobatAppDataPath in (Get-ChildItem "$DirName").Name)
+	{
+            ForEach($cachePath in $possibleCachePaths)
+            {
+                 Remove-CacheFiles "$DirName\$AcrobatAppDataPath\$cachePath"
+            }
+	}
+    } 
+}
+
+
+#------------------------------------------------------------------#
+#- Clear-LibreOfficeCacheFiles                                     #
+#------------------------------------------------------------------#
+Function Clear-LibreOfficeCacheFiles {
+    param([string]$user=$env:USERNAME)
+    $DirName="C:\users\$user\AppData\Roaming\LibreOffice"
+    if((Test-Path "$DirName"))
+    {
+        $possibleCachePaths = @('cache','crash','user\backup','user\temp')
+	ForEach($LibreOfficeAppDataPath in (Get-ChildItem "$DirName").Name)
+	{
+            ForEach($cachePath in $possibleCachePaths)
+            {
+                 Remove-CacheFiles "$DirName\$LibreOfficeAppDataPath\$cachePath"
+            }
+	}
+    } 
+}
+
+#Endregion MiscApplications
 
 #------------------------------------------------------------------#
 #- MAIN                                                            #
