b769b565acf98615726139079c81c1d9bb9ccc1c
Raivanx <xavimane@gmail.com>
2021-12-24 13:23:58 +0100

    Added fucntions to clear the windows updates data and cache.

--- DriveClean.ps1
+++ DriveClean.ps1
@@ -64,6 +64,73 @@ Function Clear-WindowsUserCacheFiles {
     Remove-Dir "C:\Users\$user\AppData\Local\Temp"
 }
 
+#------------------------------------------------------------------#
+#- Clear-WUS-Cache Group                                           #
+#------------------------------------------------------------------#
+
+function CheckWUS() {
+    $s = Get-Service wuauserv
+    if ($s.Status -eq "Running") {
+        return 0
+    } else {
+        return 1
+    }
+}
+function StopWUS() {
+     Stop-Service wuauserv -Force
+}
+
+function StartWUS() {
+    Start-Service wuauserv
+}
+
+function WUSRunning() {
+    Write-Host "Windows Update Service is Running..." -ForegroundColor Red
+    Write-Host " Stopping Windows Update Service..." -ForegroundColor Blue
+
+    # Stopping Windows Update Service and check again if it is stopped
+    StopWUS
+    if (CheckWUS) {
+        WUSStopped
+    } else {
+        Write-Host "Can't stop Windows Update Service..." -ForegroundColor Red
+        exit 1
+    }
+    Write-Host "Starting Windows Update Service..." -ForegroundColor Blue
+
+    # Starting the Windows Update Service again
+    StartWUS
+}
+function WUSStopped() {
+    Write-Host "Windows Update Service is Stopped..." -ForegroundColor Green
+
+    # Getting free disk space before the cleaning actions
+    Write-Host " Free Disk Space before: " -ForegroundColor Blue -NoNewline
+    $Before = FreeDiskSpace
+    Write-Host "$Before GB"
+
+    Write-Host " Cleaning Files..." -ForegroundColor Blue -NoNewline
+    Get-ChildItem -LiteralPath $env:windir\SoftwareDistribution\Download\ -Recurse | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+    Write-Host "Done..." -ForegroundColor Green
+
+    # Getting free disk space after the cleaning actions
+    Write-Host " Free Disk Space after: " -ForegroundColor Blue -NoNewline
+    $After = FreeDiskSpace
+    Write-Host "$After GB"
+
+    # Calculating the free disk space difference
+    Write-Host " Cleaned: " -ForegroundColor Blue -NoNewline
+    $Cleaned = $After - $Before
+    Write-Host "$Cleaned GB"
+}
+
+# Program
+if (CheckWUS) {
+    WUSStopped
+} else {
+    WUSRunning
+}
+
 #Region HelperFunctions
 
 #------------------------------------------------------------------#
